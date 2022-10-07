8e6c054925e851506ca2f00a354303c9d72dbb4e
theluposki <theluposki@gmail.com>
2022-10-07 14:35:11 -0300

    correct

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ import { search } from "./SearchRegex.js"
 import { getTotal } from "./sumTotal.js"
 
 const peaple = [
-  { name: "Tomas Shelb" , balance: "30000", qtd: "w2" },
+  { name: "Tomas Shelb" , balance: "30000", qtd: "2" },
   { name: "Daenerys" , balance: 30000, qtd: 2 },
   { name: "John Snow" , balance: 30000, qtd: "2" },
   { name: "Alina Stark" , balance: 30000, qtd: 2 },
 ]
 
-console.log(search("D", peaple))
-console.log(dateFormat(Date.now(), { dateStyle: 'medium', timeStyle: 'medium' }))
-console.log(currency(2000))
+console.log("\n Search: \n",search("T", peaple))
+console.log("\n DateFormat: \n",dateFormat(Date.now(), { dateStyle: 'medium', timeStyle: 'medium' }))
+console.log("\n Currency: ",currency(2000))
 
-console.log("\n",getTotal(peaple, "balance", "qtd"), "\n")
+console.log("\n SumTotal: \n",getTotal(peaple, "balance", "qtd"), "\n")
--- sumTotal.js
+++ sumTotal.js
@@ -1,12 +1,8 @@
 export const getTotal = (arr, prop, prop2) => {
-    arr.forEach(el => {
-      if(isNaN(Number(el[prop]))) return `the property passed [ ${prop} ] is not a number`
-      if(isNaN(Number(el[prop2]))) return `the property passed [ ${prop2} ] is not a number`
-    });
-    
-    return arr.reduce((a, b) => {
-      if(prop2) return a + Number(b[prop]) * Number(b[prop2])  
-      
-      return a + Number(b[prop])
-    }, 0) 
-}
+  
+  return arr.reduce((a, b) => {
+    if (prop2) return a + Number(b[prop]) * Number(b[prop2]);
+
+    return a + Number(b[prop]);
+  }, 0);
+};
